798e1c6e520698ace7c620c0d0e83729c86cf3f2
hipolito <huaylla7@hotmail.com>
2023-07-30 00:20:44 -0400

    Creación del Proyecto

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Ei02m6 Registro Consumo"
+include(":app")
+ 
